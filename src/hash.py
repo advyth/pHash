3d2f018d8e85b02ee434b9ed852f25feac92f8e2
advyth <advythashok@gmail.com>
2019-03-05 14:05:11 +0530

    Level 2 hash added

--- src/hash.py
+++ src/hash.py
@@ -10,12 +10,23 @@ for i in range(0, len(n)):
 	else:
 		num_array.append(ord(n[i]))
 
-def pHash(c_array):
+def pHash_1(c_array):
 	h = 0
 	for i in range(0, len(c_array)):
 		h = h + math.log(c_array[i])
-	print(int((h + math.log(len(c_array))/len(c_array)+1) * 1000000000000000))
+	h1 = int((h + math.log(len(c_array))/len(c_array)+1) * 1000000000000000)
+	pHash_2(h1)
 
-pHash(num_array)
+def pHash_2(hash_1):
+	h1_split = list(str(hash_1))
+	h2 = 0
+	for i in range(0, len(h1_split)):
+		if int(h1_split[i]) == 0:
+			h2 += 0
+		else:
+			h2 += math.log(int(h1_split[i]))
+	print("Hash value is " + str(int(h2 * 100000000000000)))
+	
+pHash_1(num_array)
 	
 
